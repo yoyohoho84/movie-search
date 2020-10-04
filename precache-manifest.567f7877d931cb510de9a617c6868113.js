22d90f0086866704bc0214fa35f014bab9cb6f79
yoyohoho84 <kirill_1131@mail.ru>
2020-10-05 00:29:15 +0300

    Updates

---
+++ precache-manifest.567f7877d931cb510de9a617c6868113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a649676ee0470e375024e097b9e7a5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b697b88c8de3b984b861",
+    "url": "./static/css/main.4e23d75f.chunk.css"
+  },
+  {
+    "revision": "bbe0c65b5ca573fb05bb",
+    "url": "./static/js/2.10148dd8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.10148dd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b697b88c8de3b984b861",
+    "url": "./static/js/main.1dacf077.chunk.js"
+  },
+  {
+    "revision": "ef8a0e5f462db8d67cc5",
+    "url": "./static/js/runtime-main.013bcf70.js"
+  }
+]);
--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export * from './Header';
-export * from './SearchInput';
-export * from './FilmsList';
-export * from './TagsList';
-
